9458dec15e10cd417c645faa0a47eb1f2b40d0bb
Chris Hsu <chrishsu@berkeley.edu>
2014-10-22 23:10:02 -0700

    Added sampling and other infrastructure

--- Makefile
+++ Makefile
@@ -1,12 +1,11 @@
 CC = g++
-CFLAGS = -Wall -I ./eigen/ ./Sampler.h
+CFLAGS = -Wall -I ./eigen/
 LDFLAGS = stuff
-	
-RM = /bin/rm -f 
-all: main 
-main: RayTracer.cpp  
+
+RM = /bin/rm -f
+all: main
+main: RayTracer.cpp
 	$(CC) $(CFLAGS) RayTracer.cpp -o raytracer
 
-clean: 
+clean:
 	rm -rf raytracer
-
--- RayTracer.cpp
+++ RayTracer.cpp
@@ -6,35 +6,78 @@
 #include <Eigen/Dense>
 #include <time.h>
 #include <math.h>
-#include "Sampler.h"
 
 using namespace std;
 using namespace Eigen;
 
-class Scene {
-	public: 
-		Scene(Vector3d pEye_Vector, MatrixXd pImage_plane, Vector2i pPixel_dimensions) {
-			eye_Vector = pEye_Vector;
-			image_plane = pImage_plane;
-			pixel_dimensions = pPixel_dimensions;
-			mySampler = NULL;
-		}
-	private:
-		Vector3d eye_Vector;
-		MatrixXd image_plane;
-		Vector2i pixel_dimensions;
-		Sampler mySampler;
-		// Camera myCamera;
-		// RayTracer myRayTracer;
-		// Film myFilm;
-
+class Sample {
+	public:
+		int x, y;
+		Sample(int x_in, int y_in);
 };
 
+Sample::Sample(int x_in, int y_in){
+	x = x_in;
+	y = y_in;
+};
 
+class Sampler {
+	int x_dim, y_dim, current_x, current_y;
+	bool done;
+	public:
+		Sampler(int x, int y);
+		bool getSample(Sample* sample);
+	private:
+};
+
+Sampler::Sampler(int x, int y){
+	x_dim = x;
+	y_dim = y;
+	current_x = -1; //Off by 1 error hack
+	current_y = 0;
+	done = false;
+};
+
+bool Sampler::getSample(Sample* sample){
+	if (done == true){
+		return false;
+	}
+	else if (current_x < x_dim){
+		current_x += 1;
+	}
+	else if (current_x == x_dim && current_y < y_dim){
+		current_y = current_y + 1;
+		current_x = 0;
+	}
+	else if (current_x == x_dim && current_y == y_dim){
+		done = true;
+	}
+	sample->x = current_x;
+	sample->y = current_y;
+	return true;
+};
+
+class Scene {
+	Sampler mySampler;
+	public:
+		Scene(int output_x, int output_x);
+		void render();
+	private:
+};
+
+Scene::Scene() : mySampler(int output_x, int output_y){
+};
+
+void Scene::render() {
+	Sample sample (0,0);
+	printf("Scene render called\n");
+	while (mySampler.getSample(&sample)) {
+		printf("X: %d Y: %d\n", sample.x, sample.y);
+	}
+};
 
 int main(int argc, char *argv[]) {
-  MatrixXd m = MatrixXd::Random(3,3);
-  return 0;
+	Scene myScene;
+	myScene.render();
+	return 0;
 }
-
-
