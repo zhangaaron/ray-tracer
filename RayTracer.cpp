cb8620137c970c02df5f37952d737913c83bf0f7
Chris Hsu <chrishsu@berkeley.edu>
2014-10-23 00:17:03 -0700

    More classes, about to start ray generation

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -21,6 +21,23 @@ Sample::Sample(int x_in, int y_in){
 	y = y_in;
 };
 
+class Ray {
+	Vector2f pos;
+	Vector3f dir;
+	float t_min, t_max;
+	public:
+		Ray(Vector2f pos_input, Vector3f dir_input, float min_t, float max_t);
+	private:
+};
+
+Ray::Ray(Vector2f pos_input, Vector3f dir_input, float min_t, float max_t){
+	pos = pos_input;
+	dir = dir_input;
+	t_min = min_t;
+	t_max = max_t;
+};
+
+
 class Sampler {
 	int x_dim, y_dim, current_x, current_y;
 	bool done;
@@ -57,27 +74,69 @@ bool Sampler::getSample(Sample* sample){
 	return true;
 };
 
+class Film {
+	int x, y;
+	MatrixXf output_image_r;
+	MatrixXf output_image_g;
+	MatrixXf output_image_b;
+	public:
+		Film(int output_x, int output_y);
+		void commit(Sample& sample, Array3f color);
+		void writeImage();
+	private:
+};
+
+Film::Film(int output_x, int output_y){
+	x = output_x;
+	y = output_y;
+	MatrixXf output_image_r(output_x, output_y);
+	MatrixXf output_image_g(output_x, output_y);
+	MatrixXf output_image_b(output_x, output_y);
+}
+
+void Film::commit(Sample& sample, Array3f color){
+	output_image_r(sample.x, sample.y) = color[0];
+	output_image_g(sample.x, sample.y) = color[1];
+	output_image_b(sample.x, sample.y) = color[2];
+}
+
+void writeImage(){
+	//Figure out how to write to image
+}
+
+class Camera {
+	Vector3f camera_coord;
+	public:
+		Camera(Vector3f coord);
+		void generateRay(Sample& sample, Ray* ray);
+	private:
+};
+
+Camera::Camera(Vector3f coord){
+	camera_coord = coord;
+};
+
 class Scene {
 	Sampler mySampler;
+	Film myFilm;
 	public:
-		Scene(int output_x, int output_x);
+		Scene(int output_x, int output_y);
 		void render();
 	private:
 };
 
-Scene::Scene() : mySampler(int output_x, int output_y){
+Scene::Scene(int output_x, int output_y) : mySampler(output_x, output_y), myFilm(output_x, output_y){
 };
 
 void Scene::render() {
 	Sample sample (0,0);
-	printf("Scene render called\n");
 	while (mySampler.getSample(&sample)) {
 		printf("X: %d Y: %d\n", sample.x, sample.y);
 	}
 };
 
 int main(int argc, char *argv[]) {
-	Scene myScene;
+	Scene myScene(100, 100);
 	myScene.render();
 	return 0;
 }
