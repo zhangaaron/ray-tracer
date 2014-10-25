2821e78c1ded64882611f576373cd11e55a970cb
zhangaaron <aaron.zhang@berkeley.edu>
2014-10-25 14:57:45 -0700

    Fixed problem with sampler

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -48,40 +48,30 @@ Ray::Ray(Vector3f pos_input, Vector3f dir_input, float min_t, float max_t){
 
 class Sampler {
 	public:
-		int x_dim, y_dim, current_x, current_y;
-		bool done;
+		int x_max, y_max;
+		int x, y;
+		Sampler(int x_max, int y_max){
+			this->x_max = x_max;
+			this->y_max= y_max;
+			x = 0;
+			y = 0;
+		};
+		bool hasNext();
+		void next(int *XYCoords);
 
-		Sampler(int x, int y);
-		bool getSample(Sample* sample);
 	private:
 };
 
-Sampler::Sampler(int x, int y){
-	x_dim = x;
-	y_dim = y;
-	current_x = -1; //Off by 1 error hack
-	current_y = 0;
-	done = false;
+bool Sampler::hasNext(){
+	return (x < x_max && y < y_max);
+}
+void Sampler::next(int* XYCoords){	
+	XYCoords[0] = x;
+	XYCoords[1] = y;
+	x = (x + 1) % x_max;
+	if (x == 0)	y++;
 };
 
-bool Sampler::getSample(Sample* sample){
-	if (done == true){
-		return false;
-	}
-	else if (current_x < x_dim){
-		current_x++;
-	}
-	else if (current_x == x_dim && current_y < y_dim){
-		current_y++;
-		current_x = 0;
-	}
-	else if (current_x == x_dim && current_y == y_dim){
-		done = true;
-	}
-	sample->x = current_x;
-	sample->y = current_y;
-	return true;
-};
 
 //Bit packed struct for color, RGB values 0-255
 struct Color {
@@ -93,11 +83,10 @@ class Film {
 
 	public:
 		int width, height;
-		std::vector<unsigned char> RGBAOutputVector;
-		char * fileName;
-
+		char *fileName;
+		unsigned char *RGBOutputArr;
 		Film(int output_x, int output_y, char *fileName);
-		void commit(Sample *sample, Color *color);
+		void commit(int *XYCoords, Color *color);
 		void writeImage();
 	private:
 };
@@ -107,22 +96,22 @@ Film::Film(int output_x, int output_y, char *fileName){
 	width = output_x;
 	height = output_y;
 	this->fileName = fileName;
-	RGBAOutputVector.resize(width * height * 4);
-	for (int i = 3; i < width * height * 4; i += 4) {
-		RGBAOutputVector[i] = 255; //Initialize opacity value of every pixel to 255. 
-	}
+	RGBOutputArr = (unsigned char *)malloc(width * height * 3);
 }
 
-void Film::commit(Sample *sample, Color *color){
+void Film::commit(int *XYCoords, Color *color){
 	//Index into our output array and assign the correct color.
-	RGBAOutputVector[sample->x + sample->y * width * 4] = color->R;
-	RGBAOutputVector[sample->x + sample->y * width * 4 + 1] = color->G;
-	RGBAOutputVector[sample->x + sample->y * width * 4 + 2] = color->B;
+	int arrayLoctoWrite = (XYCoords[0] + XYCoords[1]) * 3;
+	RGBOutputArr[arrayLoctoWrite] = color->R;
+	RGBOutputArr[arrayLoctoWrite + 1] = color->G;
+	RGBOutputArr[arrayLoctoWrite + 2] = color->B;
+	//printf("%d, %d, %d, written at %d ", (int)color->R, (int)color->G, (int)color->B, arrayLoctoWrite);
 }
 
 void Film::writeImage(){
-	encode(fileName, RGBAOutputVector, width, height);
-}
+
+lodepng_encode24_file(fileName ,RGBOutputArr, width, height);
+}                            
 
 class Camera {
 	public:
@@ -135,7 +124,7 @@ class Camera {
 		int output_y;
 
 		Camera(Vector3f coord, Vector3f lleft, Vector3f lright, Vector3f ulleft, Vector3f uright, int x, int y);
-		void generateRay(Sample *sample, Ray* ray);
+		void generateRay(int *XYCoords, Ray* ray);
 	private:
 };
 
@@ -148,10 +137,9 @@ Camera::Camera(Vector3f coord, Vector3f lleft, Vector3f lright, Vector3f ulleft,
 	output_x = x;
 	output_y = y;
 };
-
-void Camera::generateRay(Sample *sample, Ray* ray){
-	float u = sample->x / ((float)output_x);
-	float v = sample->y / ((float)output_y);
+void Camera::generateRay(int *XYCoords, Ray* ray){
+	float u = XYCoords[0] / ((float)output_x);
+	float v = XYCoords[1] / ((float)output_y);
 	//printf("u: %d %f\tv: %d %f\n", sample.x, u, sample.y, v);
 	Vector3f P = u*(v*ll + (1-v)* ul) + (1-u)*(v*lr + (1-v) * ur);
 	ray->pos = camera_coord;
@@ -171,22 +159,27 @@ class Scene {
 	private:
 };
 
-Scene::Scene(Vector3f cam_coord, Vector3f ll, Vector3f lr, Vector3f ul, Vector3f ur, int output_x, int output_y, char *fileName) : mySampler(output_x, output_y), myFilm(output_x, output_y, fileName), myCamera(cam_coord, ll, lr, ul, ur, output_x, output_y){
+Scene::Scene(Vector3f cam_coord, Vector3f ll, Vector3f lr, Vector3f ul, Vector3f ur, 
+			int output_x, int output_y, char *fileName) 
+ : mySampler(output_x, output_y), myFilm(output_x, output_y, fileName),
+  myCamera(cam_coord, ll, lr, ul, ur, output_x, output_y){
 };
 
 void Scene::render() {
 	Sample sample (0,0);
 	Ray ray(0, 1);
-	while (mySampler.getSample(&sample)) {
-		myCamera.generateRay(&sample, &ray);
+	while (mySampler.hasNext()) {
+		int XYCoords[2];
+		 mySampler.next(XYCoords);
+		myCamera.generateRay(XYCoords, &ray);
 		// printf("Ray at X: %d Y: %d\n", sample.x, sample.y);
 		// printf("Pos: (%f, %f, %f)\tDirection: (%f, %f, %f)\n\n\n", ray.pos[0], ray.pos[1], ray.pos[2], ray.dir[0], ray.dir[1], ray.dir[2]);
 		struct Color tempColor;
-		tempColor.R = sample.x + sample.y;
-		tempColor.G = sample.x + sample.y;
-		tempColor.B = sample.x + sample.y;
-		myFilm.commit(&sample, &tempColor);
-	}
+		tempColor.R = (char)(XYCoords[0] + XYCoords[1]);
+		tempColor.G = (char)(XYCoords[0] + XYCoords[1]);
+		tempColor.B = (char)(XYCoords[0] + XYCoords[1]);
+		myFilm.commit(XYCoords, &tempColor);
+		}
 	myFilm.writeImage();
 };
 
@@ -196,10 +189,12 @@ int main(int argc, char *argv[]) {
 	Vector3f lr(50, -100, -50);
 	Vector3f ul(50, 100, 50);
 	Vector3f ur(50, -100, 50);
-	char output[] = "Hello.png";
+	char *output = ./helloworld.png;
 	Scene myScene(cam_coord, ll, lr, ul, ur, 200, 100, output);
 
-	myScene.render();
+	 myScene.render();
+ 	//unsigned char RGBOutputArr[] = {(char)255, (char)0, (char)0,(char)255, (char)0, (char)0,(char)255, (char)0, (char)0,(char)255, (char)0, (char)0};
+	//lodepng_encode24_file("./hello" ,RGBOutputArr, 2, 2);
 
 	return 0;
 }
