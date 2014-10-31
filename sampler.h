92459a3c3d7d6eddc7408a6f87b27b85ecc25126
zhangaaron <aaron.zhang@berkeley.edu>
2014-10-30 21:07:22 -0700

    Moved some files around

---
+++ sampler.h
@@ -0,0 +1,35 @@
+#pragma once 
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <stdlib.h>
+#include <Eigen/Dense>
+#include <time.h>
+#include <math.h>
+
+class Sampler {
+	public:
+		int x_max, y_max;
+		int x, y;
+		Sampler(int x_max, int y_max){
+			this->x_max = x_max;
+			this->y_max= y_max;
+			x = 0;
+			y = 0;
+		};
+		bool hasNext();
+		void next(int *XYCoords);
+
+	private:
+};
+
+bool Sampler::hasNext(){
+	return (x < x_max && y < y_max);
+}
+void Sampler::next(int* XYCoords){
+	XYCoords[0] = x;
+	XYCoords[1] = y;
+	x = (x + 1) % x_max;
+	if (x == 0)	y++;
+};
